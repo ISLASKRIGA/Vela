be13cea6611bd87cc4c9e28a06235d044bace20e
chuch <chuch@LAPTOP-RG44R8BT>
2023-08-23 20:04:25 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vela
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int valorDeVela = random.Next(100, 801); // Generar valor aleatorio entre 100 y 800
+            Console.WriteLine(valorDeVela);
+
+            if (valorDeVela >= 400)
+            {
+                Console.WriteLine("La vela está encendida.");
+            }
+            else
+            {
+                Console.WriteLine("La vela está apagada.");
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
